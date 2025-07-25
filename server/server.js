ee5f71d5dd430c89fe350ccd5371d748ea1d7f3e
lakshaygupta2004 <laksh8403@gmail.com>
2025-07-25 13:52:50 +0530

    fix: add tailwindcss, postcss, and autoprefixer

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}
+));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
